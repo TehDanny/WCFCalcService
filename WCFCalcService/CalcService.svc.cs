33caf2d21672d4fcb90c2e36e6b44b0f65047c16
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-09-22 14:04:54 +0200

    Completed first web service exercise

---
+++ WCFCalcService/CalcService.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFCalcService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalcService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CalcService.svc or CalcService.svc.cs at the Solution Explorer and start debugging.
+    public class CalcService : ICalcService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public IntDivisionResult Divide(int a, int b)
+        {
+            IntDivisionResult result = new IntDivisionResult();
+            result.Kvotioent = a / b;
+            result.Rest = a % b;
+            return result;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ WCFCalcService/ICalcService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFCalcService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalcService" in both code and config file together.
+    [ServiceContract]
+    public interface ICalcService
+    {
+        [OperationContract]
+        int Add(int a, int b);
+
+        [OperationContract]
+        int Substract(int a, int b);
+
+        [OperationContract]
+        int Multiply(int a, int b);
+
+        [OperationContract]
+        IntDivisionResult Divide(int a, int b);
+    }
+}
